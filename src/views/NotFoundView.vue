5aff07e5c2df59042a0ff4eb5d87c13ab8767be8
Carmenppp <estelaparracarmen@gmail.com>
2023-07-25 12:14:44 -0400

    Se agrega el projecto

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useRouter } from 'vue-router';
+  const router = useRouter()
+  const volver = () => {
+    router.push(`/pokemons`)
+  }
+
+</script>
+
+<template>
+    <h1>404</h1>
+    <button @click="volver" class="btn btn-outline-primary">Volver a pagina</button>
+</template>
---
+++ src/views/PokeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+import { useGetData } from '../composables/getData';
+
+const route = useRoute()
+const router = useRouter()
+const {data, getData, loading, error} = useGetData()
+
+
+const back = () => {
+    router.push(`/pokemons`)
+}
+
+getData(`https://pokeapi.co/api/v2/pokemon/${route.params.poke}`)
+
+</script>
+
+<template>
+    <p v-if="loading">Cargando informacion...</p>
+    <div class="alert alert-danger mt-3" v-if="error"> {{ error }} </div>
+    <div v-if="data">
+        <img :src="data.sprites?.front_default" alt="">
+       <h1>Poke name: {{ $route.params.poke }}</h1>
+    </div>
+    <button @click="back" class="btn btn-outline-primary">Volver</button>
+</template>
